61cfce0c9b89a66089ea4b14f02c793162be16f9
emdehateam <96113872+emdehateam@users.noreply.github.com>
2021-12-14 17:22:11 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import streamlit as st
-
+st.set_page_config(layout="wide")
 st.sidebar.header("Welcome streamlit web view")
 st.header("dengan saya dani")
 dataMinyak = pd.read_csv('produksi_minyak_mentah.csv')
 if st.checbox("Lihat data"):
-  st.create(dataMinyak)
+  st.write(dataMinyak)
